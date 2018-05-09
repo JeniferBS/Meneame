c0b6b2b72fc3fe085001f427a27725f74bd60c0a
Miguel <migohe14@gmail.com>
2018-05-09 19:04:27 +0200

    rutes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+   <pp-header></pp-header>
+   <router-view></router-view>
+   <pp-footer></pp-footer>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 export default {
-  name: 'App'
+  components: {
+    ppHeader: Header,
+    ppFooter: Footer
+
+  }
+
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../components/Home'
+import Login from '../components/Login'
+import Register from '../components/Register'
 
 Vue.use(Router)
 
@@ -8,8 +10,21 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
+
   ]
 })
